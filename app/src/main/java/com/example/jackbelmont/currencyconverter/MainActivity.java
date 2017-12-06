7379edbbfcebf8423ba00c966ea87617e91e84b9
Patrick <agrip@bu.edu>
2017-12-06 13:12:35 -0500

    Added Button feature that takes user to a second screen

--- app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.jackbelmont.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Defines button which we will use
+    Button buttonMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
     }
+
+
+    //creates the listener on the button
+    public void addListenerOnButton()
+    {
+            final Context context = this;
+
+            buttonMain = (Button) findViewById(R.id.button);
+
+        buttonMain.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context,MainActivity2.class);
+
+                        startActivity(intent);
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.jackbelmont.currencyconverter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+/**
+ * Created by Patrick on 12/6/2017.
+ */
+
+//This is the main activity for the button
+public class MainActivity2 extends MainActivity
+{
+    Button buttonMain;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
+    }
+}
