8ac30eb6dba03b4338619ea68cf1f78aa0edf474
Patrick <agrip@bu.edu>
2017-12-07 22:37:00 -0500

    Cleanup of files, removed MainActivity2.java because it is no longer used.
    Cleaned up some commented out commands

--- app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
@@ -57,33 +57,18 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View arg0)
             {
 
-
-                //will take in the value put in by the edit text space
-               /* EditText editText = (EditText) findViewById(R.id.editText2);
-                String value = editText.getText().toString();
-
-                //outputs the value in activity_main2
-                setContentView(R.layout.activity_main2);
-
-                TextView textView = (TextView) findViewById(R.id.textView4);
-                textView.setText(value);  */
-
-                //This gives a feature for returning to the main page but there is also
-                //a bug so until it is fixed we will keep it this way
-
-
                 //Gets whatever was inputted into main text box
                 EditText editText = (EditText) findViewById(R.id.editText2);
                 String string1 = editText.getText().toString();
 
 
+                //sets up content layout
                 setContentView(R.layout.activity_main2);
 
 
                 //Puts value into text box 8
                 TextView textView = (TextView) findViewById(R.id.textView8);
                 textView.setText(string1);
-
 
                 //Puts value into text box 9
                 TextView textView1 = (TextView) findViewById(R.id.textView9);
@@ -101,9 +86,6 @@ public class MainActivity extends AppCompatActivity
                 TextView textView4 = (TextView) findViewById(R.id.textView12);
                 textView4.setText(string1);
 
-                //Intent intent = new Intent(context,MainActivity2.class);
-
-                //startActivity(intent);
             }
 
 
