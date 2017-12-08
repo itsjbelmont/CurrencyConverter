9e679d61d17f8234da939d9df363ea445c8317b1
Patrick <agrip@bu.edu>
2017-12-07 22:19:40 -0500

    Changed layout of main screen so that the main screen now just inputs the beginning currency.
    The second page will list the equivalent output in the various currencies.
    
    For now everything works, but back button will need to be fixed or a new one will need to be addded.

--- app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.EditText;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 
 //Main public class which contains all the Main Activity
 public class MainActivity extends AppCompatActivity
@@ -57,22 +59,51 @@ public class MainActivity extends AppCompatActivity
 
 
                 //will take in the value put in by the edit text space
-                EditText editText = (EditText) findViewById(R.id.editText2);
+               /* EditText editText = (EditText) findViewById(R.id.editText2);
                 String value = editText.getText().toString();
 
                 //outputs the value in activity_main2
                 setContentView(R.layout.activity_main2);
 
                 TextView textView = (TextView) findViewById(R.id.textView4);
-                textView.setText(value);
+                textView.setText(value);  */
 
                 //This gives a feature for returning to the main page but there is also
                 //a bug so until it is fixed we will keep it this way
-                /*
-                Intent intent = new Intent(context,MainActivity2.class);
 
-                startActivity(intent);
-                */
+
+                //Gets whatever was inputted into main text box
+                EditText editText = (EditText) findViewById(R.id.editText2);
+                String string1 = editText.getText().toString();
+
+
+                setContentView(R.layout.activity_main2);
+
+
+                //Puts value into text box 8
+                TextView textView = (TextView) findViewById(R.id.textView8);
+                textView.setText(string1);
+
+
+                //Puts value into text box 9
+                TextView textView1 = (TextView) findViewById(R.id.textView9);
+                textView1.setText(string1);
+
+                //puts value into box 10
+                TextView textView2 = (TextView) findViewById(R.id.textView10);
+                textView2.setText(string1);
+
+                //puts value into box 11
+                TextView textView3 = (TextView) findViewById(R.id.textView11);
+                textView3.setText(string1);
+
+                //puts view into box 12
+                TextView textView4 = (TextView) findViewById(R.id.textView12);
+                textView4.setText(string1);
+
+                //Intent intent = new Intent(context,MainActivity2.class);
+
+                //startActivity(intent);
             }
 
 
--- app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity2.java
+++ app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity2.java
@@ -2,9 +2,13 @@ package com.example.jackbelmont.currencyconverter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Patrick on 12/6/2017.
@@ -13,14 +17,41 @@ import android.widget.TextView;
 //This is the main activity for the button
 public class MainActivity2 extends MainActivity
 {
-    Button buttonMain;
+    //Button buttonMain;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
 
-            super.onCreate(savedInstanceState);
+
+            /* EditText editText = (EditText) findViewById(R.id.editText2);
+            String value = editText.getText().toString();
+
+            //outputs the value in activity_main2
             setContentView(R.layout.activity_main2);
+
+            TextView textView = (TextView) findViewById(R.id.textView4);
+            textView.setText(value);  */
+
+            super.onCreate(savedInstanceState);
+
+            //Gets whatever was inputted into main text box
+            /*EditText editText = (EditText) findViewById(R.id.editText2);
+            String string1 = editText.getText().toString();
+
+
+            setContentView(R.layout.activity_main2);
+
+
+            //Puts value into text box 8
+            TextView textView = (TextView) findViewById(R.id.textView8);
+            textView.setText(string1);
+
+            //((TextView) findViewById(R.id.textView8)).setText();
+
+            //Puts value into text box 9
+            TextView textView1 = (TextView) findViewById(R.id.textView9);
+            textView1.setText(string1);*/
 
 
     }
