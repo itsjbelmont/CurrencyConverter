ed5d645528db426b13cf4f86f39887aa8af1a092
Thuc Nguyen <thucnguyen61098@gmail.com>
2017-12-10 16:05:51 -0500

    Added code to put value and currency selection from MainActivity into MainActivity2 and added 2D array with stored exchange rates

--- app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/jackbelmont/currencyconverter/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -30,44 +31,29 @@ public class MainActivity extends AppCompatActivity
     //This onCreate sets up the layout view on the app and creates a listener
     //for input from the button
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //listens for button
-        addListenerOnButton();
+        //creates the listener on the button
+        //the button will lead to another screen, and also link the data being sent to
+        //second screen
 
-    }
-
-    //creates the listener on the button
-    //the button will lead to another screen, and also link the data being sent to
-    //second screen
-    public void addListenerOnButton()
-    {
-
-            //final Context context = this;
-
-            buttonMain = (Button) findViewById(R.id.button);
-
-
-
-        //sets the button once clicked
-        buttonMain.setOnClickListener(new OnClickListener()
-        {
+        buttonMain = (Button) findViewById(R.id.button);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final EditText editText = (EditText) findViewById(R.id.editText2);
+        buttonMain.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View arg0)
-            {
-
-                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
-
+            public void onClick(View arg0) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                String value = editText.getText().toString();
+                String choice = spinner.getSelectedItem().toString();
+                intent.putExtra("VALUE", value);
+                intent.putExtra("CHOICE", choice);
                 startActivity(intent);
-
             }
-
-
         });
-
     }
 
 
